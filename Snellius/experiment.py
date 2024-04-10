96821b008fad625831a9f653db0447754976e040
unknown <wouterknibbe@gmail.com>
2024-04-10 16:00:32 +0200

    Added continuation functionality to experiment.py

--- Snellius/experiment.py
+++ Snellius/experiment.py
@@ -13,6 +13,7 @@ parser = argparse.ArgumentParser(description='Run the experiment with provided p
 parser.add_argument('sizes', type=str, help='A list of city sizes, e.g., "[10,12]"')
 parser.add_argument('ranges', type=str, help='A list of value ranges, e.g., "[10,1000]"')
 parser.add_argument('mutations', type=int, help='An integer mutations, e.g., 500')
+parser.add_argument('continuation', type=str, default="", nargs='?', help='A list of matrix continuations, e.g., "[,(50,10)]". Corresponding matrices must be in "Progress" folder')
 
 # Parse arguments
 args = parser.parse_args()
@@ -20,6 +21,11 @@ args = parser.parse_args()
 # Convert string representations of lists to actual lists
 sizes = ast.literal_eval(args.sizes)
 ranges = ast.literal_eval(args.ranges)
+if args.continuation == "":
+    continuations = []
+else:
+    continuations = [",".join(map(str, tup)) for tup in ast.literal_eval(args.continuation)]
+
 
 def custom_encoder(obj):
     """
@@ -56,6 +62,39 @@ def save_partial(results, citysize, range, time):
 
     print(f"Results saved to JSON file successfully as {file_path}\nElapsed Time: {time:.2f} seconds")
 
+def custom_decoder(obj):
+    """
+    Custom decoder function that converts specific JSON values back to their original types.
+    Converts:
+    - 'Infinity' to np.inf
+    """
+    if isinstance(obj, dict):
+        for key, value in obj.items():
+            if value == "Infinity":
+                obj[key] = np.inf
+            # elif isinstance(value, list):
+                # Convert lists back to arrays
+                # obj[key] = np.array(value)
+            elif isinstance(value, dict):
+                obj[key] = custom_decoder(value)
+    elif isinstance(obj, list):
+        for i, value in enumerate(obj):
+            if value == "Infinity":
+                obj[i] = np.inf
+            # elif isinstance(value, list):
+                # obj[i] = np.array(value)
+            elif isinstance(value, dict):
+                obj[i] = custom_decoder(value)
+    return obj
+
+
+def load_result(file_path):
+
+  # Loading the JSON file with custom decoding
+  with open(file_path, "r") as json_file:
+      loaded_results = json.load(json_file, object_hook=custom_decoder)
+
+  return loaded_results
 
 def mutate_matrix(_matrix, _upper, _print):
     matrix = _matrix.copy()
@@ -71,19 +110,35 @@ def mutate_matrix(_matrix, _upper, _print):
 
     return matrix
 
-def experiment(_cities, _ranges, _mutations):
+def experiment(_cities, _ranges, _mutations, _continuations):
     for citysize in _cities:
         for rang in _ranges:
             range_results = {}
-            hardest = 0
 
             # Record the start time
             start_time = time.time()
             
-            # initialize the matrix with ints, but convert it to a floating-point type to enable np.inf
-            matrix = np.random.randint(1,rang,((citysize, citysize))).astype(float)
-            for x in range(citysize):
-                matrix[x, x] = np.inf
+            print(f"{citysize},{rang}" in _continuations)
+            print(f"{citysize},{rang}")
+            print(_continuations)
+
+            if f"{citysize},{rang}" in _continuations:
+                try:
+                    hardest, matrix = load_result(f"Progress/continue{citysize}_{rang}.json")
+                    matrix = np.array(matrix)
+                except Exception as e:
+                    print(f"{e}\n {citysize}_{rang} matrix not loaded, generating random matrix")
+                    # initialize the matrix with ints, but convert it to a floating-point type to enable np.inf
+                    matrix = np.random.randint(1,rang,((citysize, citysize))).astype(float)
+                    for x in range(citysize):
+                        matrix[x, x] = np.inf
+                    hardest = 0
+            else:
+                matrix = np.random.randint(1,rang,((citysize, citysize))).astype(float)
+                for x in range(citysize):
+                    matrix[x, x] = np.inf
+                hardest = 0
+        
             hardest_matrix = matrix
             
             for j in range(_mutations):
@@ -108,4 +163,4 @@ def experiment(_cities, _ranges, _mutations):
                     # save to json file
                     save_partial(range_results, citysize, rang, elapsed_time)
 
-experiment(sizes, ranges, args.mutations)
+experiment(sizes, ranges, args.mutations, continuations)
