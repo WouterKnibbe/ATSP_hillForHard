3ecb58a64ab3fbd8d8764a352bf89548e5cdfc3f
Wouter Knibbe <wouterknibbe@gmail.com>
2024-04-17 09:50:03 +0200

    48h error run script

--- Snellius/experiment-job.sh
+++ Snellius/experiment-job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Set job requirements
 #SBATCH --nodes=1
-#SBATCH --ntasks=8
+#SBATCH --ntasks=10
 #SBATCH --partition=rome
-#SBATCH --time=26:00:00
+#SBATCH --time=48:00:00
  
 #Loading modules
 module load 2022
@@ -11,10 +11,10 @@ module load Python/3.10.4-GCCcore-11.3.0
 pip install numpy
 
 # Define the experiment parameters for each task
-declare -a param1=("[30]" "[30]" "[50]" "[70]" "[100]" "[30]" "[50]" "[50]")
-declare -a param2=("[10]" "[1000]" "[10]" "[10]" "[10]"  "[100]" "[100]" "[1000]")
-declare -a param3=(1500 2000 1200 900 900 2000 1500 1500)
-declare -a param4=("[(30,10)]" "[(30,1000)]" "[(50,10)]" "[(70,10)]" "[(100,10)]")
+declare -a param1=("[30]" "[30]" "[50]" "[50]" "[70]" "[70]" "[70]" "[100]" "[100]" "[100]")
+declare -a param2=("[100]" "[1000]" "[100]" "[1000]" "[10]" "[100]" "[1000]" "[10]" "[100]" "[1000]")
+declare -a param3=(2000 2000 2000 2000 2000 2000 2000 2000 2000 2000)
+declare -a param4=("[(30,100)]" "[(30,1000)]" "[(50,100)]" "[(50,1000)]" "[(70,10)]" "" "" "[(100,10)]" "" "")
 
 
 # Execute the program N times, where N=ntasks requested to SLURM. In this example we request all the cores availables on the node.
